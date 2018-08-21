75763ce92e93317fd9179a326a32eb2d354b413e
Rendy Febry <rendyfebry@gmail.com>
2018-08-21 10:09:20 +0700

    Add logger middleware

--- server.go
+++ server.go
@@ -4,16 +4,28 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
-func helloHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func homeHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Home!")
+}
+
+func userHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "User!")
 }
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", helloHandler)
+	loggerConfig := middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}
+
+	e.Use(middleware.LoggerWithConfig(loggerConfig))
+
+	e.GET("/", homeHandler)
+	e.GET("/users", userHandler)
 
 	e.Logger.Fatal(e.Start(":9000"))
 }
