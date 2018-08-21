ce1698ad0a1e16d23f04f35768ecb39550abb81c
Rendy Febry <rendyfebry@gmail.com>
2018-08-21 10:22:03 +0700

    Add JWT authorization

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -11,8 +13,42 @@ func homeHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Home!")
 }
 
+func loginHandler(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username == "jon" && password == "shhh!" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "Jon Snow"
+		claims["admin"] = true
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+
+	return echo.ErrUnauthorized
+}
+
 func userHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "User!")
+}
+
+func restricted(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.String(http.StatusOK, "Welcome "+name+"!")
 }
 
 func main() {
@@ -22,7 +58,14 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", homeHandler)
+	e.POST("/login", loginHandler)
+
 	e.GET("/users", userHandler)
+
+	// Restricted group
+	r := e.Group("/restricted")
+	r.Use(middleware.JWT([]byte("secret")))
+	r.GET("", restricted)
 
 	e.Logger.Fatal(e.Start(":9000"))
 }
